f2d7aea0d5768110f8f79d78fd9a9e66f139d9bd
Meysam Farzalian <meysam.farzalian@gmail.com>
2024-09-01 14:23:43 +0330

    fix: edit

--- src/Schema/ValidationForm.js
+++ src/Schema/ValidationForm.js
@@ -21,7 +21,11 @@ export const editCourseValidation = z.object({
     teacher: z.string().min(1, { message: "وارد کردن نام مدرس الزامی است" }),
     title: z.string().min(1, { message: "وارد کردن عنوان دوره الزامی است" }),
     category: z.string().min(1, { message: "دسته بندی انتخاب نشده است" }),
-    duration: z.string(),
-    price: z.string(),
+    duration: z.number().min(1, { message: "مدت زمان دوره را وارد کنید" }),
+    price: z.number().min(1, { message: "مبلغ دوره را وارد کنید" }),
+    description: z.string().min(1, { message: "توضیحات دوره ثبت نشده است" }),
+    number_of_chapter: z.number().min(1, { message: "تعداد فصل ها را وارد کنید" }),
+    number_of_viewer: z.number().min(1, { message: "تعداد بازدید را وارد کنید" }),
+    upload_images: z.instanceof(FileList).refine(files => files.length > 0, {message: "فایل نمی‌تواند خالی باشد"})
 
 })
